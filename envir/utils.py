94a18226384f51089af48ced7e252800399c3acb
tjacek <tjacek@student.agh.edu.pl>
2017-09-08 23:56:20 +0200

    make stochastic matrix

--- envir/utils.py
+++ envir/utils.py
@@ -11,6 +11,11 @@ class BinomialDist(object):
     def __str__(self):
         return 'p:'+str(self.p)
 
+def make_stoch_matrix(n):
+    dists=[ get_distribution(n)
+             for i in range(n)]
+    return np.array(dists)
+
 def get_distribution(n):
     dist=np.random.rand(n)
     c=sum(dist)
