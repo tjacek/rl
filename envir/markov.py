28ec432adc4cf69c22167e8ebca6ca3144daf89f
tjacek <tjacek@student.agh.edu.pl>
2017-09-12 23:34:25 +0200

    markov chain improved

--- envir/markov.py
+++ envir/markov.py
@@ -25,29 +25,27 @@ class MarkovDP(envir.Enviroment):
         new_state=np.random.choice(self.get_states(), None, p=dist_i)
         return self.rewards[old_state][new_state]
 
-class MarkovChain(envir.Enviroment):
+class MarkovChain(object):
     def __init__(self,trans,start_state=0):
-        super(MarkovChain, self).__init__(state=start_state)	
         self.trans=trans
-    
+        self.state=state
+
     def __str__(self):
         return str(self.trans)
 
-    def get_actions(self):
-        return range(self.trans.shape[0])
+    def __len__(self):
+        return self.trans.shape[0]
+    
+    def gen_seq(self,n):
+        return [ self.next_step() 
+                    for i in range(n)]
 
-    def get_states(self):
-        return range(self.trans.shape[0])
-
-    def next_step(self,action_i):
-        if(not envir.is_int(action_i)):
-            raise envir.NonIntAction(action_i)
-        old_state= self.get_current_state()
+    def next_step(self):
+        old_state= self.state
         dist_i=self.trans[old_state]
         new_state=np.random.choice(self.get_states(), None, p=dist_i)
-        print(new_state)
         self.state=new_state
-        return float(new_state==action_i)
+        return self.state
 
 def make_markov_decision(n_states,n_actions):
     trans=[utils.make_stoch_matrix(n_states)
