2aca25c94c2cca246509a426365da98d03f86634
tjacek <tjacek@agh.edu.pl>
2021-03-06 21:40:18 +0100

    fix bandit problem

--- envir/bandit.py
+++ envir/bandit.py
@@ -3,26 +3,34 @@ import envir
 
 class MultiArmBandit(envir.Enviroment):
     def __init__(self,dists):
-        super(MultiArmBandit, self).__init__(state=0)
+        self.last_action=0
         self.dists=dists
 
     def __str__(self):
-        bandit_txt=''
-        for dist_i in self.dists:
-            bandit_txt+=str(dist_i)+'\n'    
-        return bandit_txt   
+        return ",".join([ str(dist_i) for dist_i in self.dist] )  
 
     def get_actions(self):
         return range(len(self.dists))
 
     def get_states(self):
-        return [self.state] 
-     
-    def next_step(self,action_i):
-        if(not envir.is_int(action_i)):
-            raise envir.NonIntAction(action_i)
-        reward=float(self.dists[action_i]())
-        return reward
+        return [0] 
+    
+    def observe(self,action_i):
+        return 0
+
+    def act(self,action_i):
+        return self.dists[action_i]()
+
+class BinomialDist(object):
+    def __init__(self,p=0.5,n=10):
+        self.p=p
+        self.n=n
+
+    def __call__(self):        
+        return np.random.binomial(n=self.n,p=self.p)
+
+    def __str__(self):
+        return 'p:'+str(self.p)
 
 def make_binomial_bandit(n=10):
     params=[np.random.uniform()
--- envir/utils.py
+++ envir/utils.py
@@ -1,15 +1,6 @@
 import numpy as np 
 
-class BinomialDist(object):
-    def __init__(self,p=0.5,n=10):
-        self.p=p
-        self.n=n
 
-    def __call__(self):        
-        return np.random.binomial(n=self.n,p=self.p)
-
-    def __str__(self):
-        return 'p:'+str(self.p)
 
 def make_stoch_matrix(n):
     dists=[ get_distribution(n)
