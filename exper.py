0a15c727480e201925ddb61146b8434385076da2
tjacek <tjacek@agh.edu.pl>
2021-03-06 20:51:08 +0100

    move filest to envir

--- exper.py
+++ exper.py
@@ -1,15 +1,15 @@
-import q_learning,baby
+import q_learning,envir.baby
 import numpy as np
 import matplotlib.pyplot as plt
 
 def exper(n_epochs=100,window=100):
-    envir=baby.CryingBaby()
+    exper_envir=envir.baby.CryingBaby()
     q_learn=q_learning.QLearn()
     mean_rewards=[]
     for i in range(n_epochs):
-        q_learn(envir,window)
-        envir.reset()
-        rewards_i=q_learn.no_learning(envir,window)
+        q_learn(exper_envir,window)
+        exper_envir.reset()
+        rewards_i=q_learn.no_learning(exper_envir,window)
         mean_rewards.append(np.mean(rewards_i))
     plt.plot(mean_rewards)
     plt.ylabel('rewards')
--- q_learning.py
+++ q_learning.py
@@ -1,37 +1,37 @@
 import numpy as np 
-import baby
+import envir.baby
 
 class QLearn(object):
-    def __init__(self,alpha=0.5,gamma=0.9,eps=0.1):
+    def __init__(self,alpha=0.05,gamma=0.9,eps=0.1):
         self.alpha=alpha
         self.gamma=gamma
         self.eps=eps
         self.q=None
 
-    def __call__(self,envir,n_epochs=100):
-        states=envir.get_states()
-        actions=envir.get_actions()
+    def __call__(self,exper_envir,n_epochs=100):
+        states=exper_envir.get_states()
+        actions=exper_envir.get_actions()
         if(self.q is None):
             self.q=np.random.rand(len(states),len(actions))
         rewards=[]
-        obs_i=envir.observe()
+        obs_i=exper_envir.observe()
         for i in range(n_epochs):
             x=obs_i.value
             y=self.next_action(obs_i)
             action_i=actions[y]
-            r_i=envir.act(action_i)
-            obs_i=envir.observe()
+            r_i=exper_envir.act(action_i)
+            obs_i=exper_envir.observe()
             future=self.gamma*self.best_action(obs_i)  #np.argmax(q[obs_i.value])
             self.q[x,y]=(1-self.alpha)*self.q[x,y]+self.alpha*(r_i+future)
             rewards.append(r_i)
         return rewards
 
-    def no_learning(self,envir,n_epochs):
+    def no_learning(self,exper_envir,n_epochs):
         rewards=[]
         for i in range(n_epochs):
-            obs_i=envir.observe()
+            obs_i=exper_envir.observe()
             action_i=self.best_action(obs_i)
-            rewards.append(envir.act(action_i))
+            rewards.append(exper_envir.act(action_i))
         return rewards
 
     def next_action(self,state_i):
@@ -43,6 +43,6 @@ class QLearn(object):
         return np.argmax(self.q[state_i.value])
 
 if __name__ == "__main__":
-    envir=baby.CryingBaby()
+    exper_envir=envir.baby.CryingBaby()
     q_learn=QLearn()
-    print(q_learn(envir))
+    print(q_learn(exper_envir))
