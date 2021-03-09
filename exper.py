136ff16cb4a46857a3092e7253d0a58f3dca6c35
tjacek <tjacek@agh.edu.pl>
2021-03-09 20:30:32 +0100

    simulation class

--- exper.py
+++ exper.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def exper(n_epochs=1000,window=10):
     exper_envir=envir.bandit.make_binomial_bandit()
 #    exper_envir=envir.baby.CryingBaby()
-    q_learn=q_learning.QLearn()
+    q_learn=q_learning.make_simulation()#QLearn()
     rewards=q_learn(exper_envir,n_epochs)
     mean_rewards=[ np.mean(rewards[i*window:(i+1)*window]) 
                     for i in range(int(n_epochs/window))] 
--- q_learning.py
+++ q_learning.py
@@ -1,26 +1,33 @@
 import numpy as np 
 import envir.baby
 
-class QLearn(object):
-    def __init__(self,alpha=0.05,gamma=0.9,eps=0.9):
-        self.alpha=alpha
-        self.gamma=gamma
-        self.policy=EpsilonGreedy(eps)
-        self.q=None
+class Simulation(object):
+    def __init__(self,policy,update):
+        self.policy=policy
+        self.update=update
+        self.model=None
 
-    def __call__(self,exper_envir,n_epochs=100):
-        if(self.q is None):
-            self.q=np.random.rand(exper_envir.get_states(),exper_envir.get_actions() )
+    def __call__(self,s_envir,n_epochs=100):
+        if(self.model is None):
+            self.model=np.random.rand(s_envir.get_states(),s_envir.get_actions())
         rewards=[]
-        obs_i=exper_envir.observe()
+        obs_i=s_envir.observe()
         for i in range(n_epochs):
-            act_i= self.policy(obs_i,self.q)
-            r_i=exper_envir.act(act_i)
-            obs_i=exper_envir.observe()
-            future=self.gamma*best_action(obs_i,self.q)
-            self.q[obs_i,act_i]=(1-self.alpha)*self.q[obs_i,act_i]+self.alpha*(r_i+future)
+            act_i= self.policy(obs_i,self.model)
+            r_i=s_envir.act(act_i)
+            obs_i=s_envir.observe()
+            self.update(obs_i,act_i, r_i,self.model)
             rewards.append(r_i)
         return np.array(rewards)
+
+class QLearn(object):
+    def __init__(self,alpha=0.05,gamma=0.9):
+        self.alpha=alpha
+        self.gamma=gamma
+
+    def __call__(self,obs_i,act_i, r_i,q):
+        future=self.gamma*best_action(obs_i,q)
+        q[obs_i,act_i]=(1-self.alpha)*q[obs_i,act_i]+self.alpha*(r_i+future)
         
 class EpsilonGreedy(object):
     def __init__(self,eps,discount=0.9):
@@ -35,6 +42,11 @@ class EpsilonGreedy(object):
 
 def best_action(state_i,q):
     return np.argmax(q[state_i])
+
+def make_simulation():
+    update=QLearn()
+    policy=EpsilonGreedy(0.9)
+    return Simulation(policy,update)
 
 if __name__ == "__main__":
     exper_envir=envir.baby.CryingBaby()
