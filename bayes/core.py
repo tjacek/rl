eb69c704981652cdedf87e6e952170a1a2f4df1c
tjacek <tjacek@agh.edu.pl>
2023-12-06 23:56:00 +0100

    factor product

--- bayes/core.py
+++ bayes/core.py
@@ -36,6 +36,9 @@ class Factor(object):
     def variable_names(self):
         return [var_i.name for var_i in self.variables]
 
+    def marginalize(self,name:str):
+        pass
+
 class FactorTable(object):
     def __init__(self,array):
         self.array=array
@@ -56,21 +59,8 @@ def get_factor(variables,pairs):
     return Factor(variables=variables, 
                   table=FactorTable(array)) 
 
-#X=Variable(name='X', 
-#	       domian=2)
-#Y=Variable(name='Y', 
-#	       domian=2)
-#Z=Variable(name='X', 
-#	       domian=2)
 
-#table=np.zeros((2,2,2))
-#table[0][0][0]=0.08
-#table[0][1][0]=0.09
-#table[1][0][0]=0.01
-#table[1][1][0]=0.02
-#table[0][0][1]=0.31
-#table[0][1][1]=0.37
-#table[1][0][1]=0.05
-#table[1][1][1]=0.07
-#Factor(variables=[X,Y.Z],
-#	   table=table)
+def factor_product(phi:Factor,psi:Factor):
+    phi_names=phi.variable_names()
+    phi_names=phi.variable_names()
+    FactorTable(object)
