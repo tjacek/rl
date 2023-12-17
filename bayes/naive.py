3bc03eed78a9d02a2d063e0c4cebd3cb542e3cfc
tjacek <tjacek@agh.edu.pl>
2023-12-17 13:26:00 +0100

    naive

---
+++ bayes/naive.py
@@ -0,0 +1,43 @@
+from core import *
+
+C=Variable(name='C',
+	       domian=3)
+S=Variable(name='S',
+	       domian=3)
+V=Variable(name='V',
+	       domian=3)
+
+p_C=get_factor(variables=[C],
+	           pairs=[({'C':0},0.001),
+	                  ({'C':1},0.009),
+                      ({'C':1},0.009)])
+
+p_CS=get_factor(variables=[C,S],
+	              pairs=[({'C':0,'S':0},0.001),
+	                     ({'C':0,'S':1},0.009),
+	                     ({'C':0,'S':2},0.990),
+	                     ({'C':1,'S':0},0.200),
+	                     ({'C':1,'S':1},0.750),
+	                     ({'C':1,'S':2},0.050),
+	                     ({'C':2,'S':0},0.800),
+	                     ({'C':2,'S':1},0.199),
+	                     ({'C':2,'S':2},0.001)])
+
+
+p_CV=get_factor(variables=[C,V],
+	              pairs=[({'C':0,'V':0},0.2),
+	                     ({'C':0,'V':1},0.2),
+	                     ({'C':0,'V':2},0.6),
+	                     ({'C':1,'V':0},0.5),
+	                     ({'C':1,'V':1},0.4),
+	                     ({'C':1,'V':2},0.1),
+	                     ({'C':2,'V':0},0.4),
+	                     ({'C':2,'V':1},0.4),
+	                     ({'C':2,'V':2},0.2)])
+
+graph=SimpleDiGraph(3)
+graph.add_edge(0,1)
+graph.add_edge(0,2)
+BayesNet(variables=[C,S,V],
+	     factors=[p_C,p_CS,p_CV],
+	     graph=graph)
