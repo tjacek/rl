1497b912deabb67eaf90bd75c67ea0d18eaacb5a
tjacek <tjacek@agh.edu.pl>
2023-12-19 15:36:54 +0100

    condition example

--- bayes/example.py
+++ bayes/example.py
@@ -1,32 +1,57 @@
 from core import *
 
-B=Variable(name='B',
+X=Variable(name='X',
 	       domian=2)
-S=Variable(name='S',
+Y=Variable(name='Y',
 	       domian=2)
-E=Variable(name='E',
+Z=Variable(name='Z',
 	       domian=2)
-D=Variable(name='D',
-	       domian=2)
-C=Variable(name='C',
-	       domian=2)
-vars=[B, S, E, D, C]
 
-#factors=[get_factor(variables=[D,E],
-#	                pairs=[({'D':0,'E':0},0.96),
-#	                       ({'D':0,'E':1},0.03),
-#	                       ({'D':1,'E':0},0.04),
-#	                       ({'D':1,'E':1},0.97)])]
+def marg_test():
+    factor=get_factor(variables=[X,Y,Z],
+	              pairs=[({'X':0,'Y':0,'Z':0},0.08),
+	                     ({'X':0,'Y':0,'Z':1},0.31),
+	                     ({'X':0,'Y':1,'Z':0},0.09),
+	                     ({'X':0,'Y':1,'Z':1},0.37),
+	                     ({'X':1,'Y':0,'Z':0},0.01),
+	                     ({'X':1,'Y':0,'Z':1},0.05),
+	                     ({'X':1,'Y':1,'Z':0},0.02),
+	                     ({'X':1,'Y':1,'Z':1},0.07), ])
+    print("Marginalize")
+    print(factor)
+    print(factor.marginalize('Y'))
 
-#get_factor(variables=[B],
-#	                pairs=[({'B':0},0.99),
-#	                       ({'B':1},0.01)])]
+def product_test():
+    phi=get_factor(variables=[X,Y],
+	              pairs=[({'X':0,'Y':0},0.3),
+	                     ({'X':0,'Y':1},0.4),
+	                     ({'X':1,'Y':0},0.2),
+	                     ({'X':1,'Y':1},0.1)])
 
-#graph=SimpleDiGraph(5)
-#graph.add_edge(1,3)
-#graph.add_edge(2,3)
-#graph.add_edge(3,4)
-#graph.add_edge(3,5)
-#bn=BayesNet(variables=vars,
-#	        factors=factors,
-#	        graph=graph)
+    psi=get_factor(variables=[Y,Z],
+	              pairs=[({'Y':0,'Z':0},0.2),
+	                     ({'Y':0,'Z':1},0.0),
+	                     ({'Y':1,'Z':0},0.3),
+	                     ({'Y':1,'Z':1},0.5)])
+
+    print("PRODUCT")
+    product= factor_product(phi,psi)
+    print(product)
+
+def cond_test():
+    phi=get_factor(variables=[X,Y,Z],
+	               pairs=[({'X':0,'Y':0,'Z':0},0.08),
+	                      ({'X':0,'Y':0,'Z':1},0.31),
+	                      ({'X':0,'Y':1,'Z':0},0.09),
+	                      ({'X':0,'Y':1,'Z':1},0.37),
+                          ({'X':0,'Y':0,'Z':0},0.01),
+	                      ({'X':0,'Y':0,'Z':1},0.05),
+	                      ({'X':0,'Y':1,'Z':0},0.02),
+	                      ({'X':0,'Y':1,'Z':1},0.07)])
+    phi= phi.condition(evidence={'Y':1})
+    print('CONDITION')
+    print(phi)
+
+marg_test()
+product_test()
+cond_test()
