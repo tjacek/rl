bae9d7c07d7556e4af9a35c4090ab270950c0a08
tjacek <tjacek@agh.edu.pl>
2024-01-04 02:09:34 +0100

    weighted sampling

--- bayes/infer.py
+++ bayes/infer.py
@@ -8,20 +8,53 @@ class DirectSampling(object):
         query_set=set([var_i.name for var_i in query])
         s_vars=[var_i for var_i in bn.variables
                   if(var_i.name in query_set)]
-        table=core.get_factor(variables=s_vars,#bn.variables,
+        table=core.get_factor(variables=s_vars,
                               pairs=None)
         for i in range(self.m):
             a_i=bn.rand()       
             eq_i=[ a_i[name_j]==value_j 
                     for name_j,value_j in evidence.items()]
             if(all(eq_i)):
-                b_i=a_i.select(variables=query)
-                hist_i=table.get(b_i)
-                table.set(b_i,hist_i+1)
+                t_i=a_i.select(variables=query)
+                hist_i=table.get(t_i)
+                table.set(t_i,hist_i+1)
+            b_i=a_i.select(variables=query)
+            hist_i=table.get(b_i)
+            table.set(b_i,hist_i+1)
         factor=core.Factor(variables=s_vars,
                            table=table)
         return factor.normalize()
 
+class LikelihoodWeightedSampling(object):
+    def __init__(self,m=10):
+        self.m=m
+
+    def __call__(self,bn:core.BayesNet,query:list,evidence:core.Assig):
+        ordering = bn.graph.topological_sort()
+        query_set=set([var_i.name for var_i in query])
+        s_vars=[var_i for var_i in bn.variables
+                  if(var_i.name in query_set)]
+        table=core.get_factor(variables=s_vars,
+                              pairs=None)
+        for i in range(self.m):
+            a_i,w_i=bn.rand(),1.0
+            for j in ordering:
+                name_j=bn.variables[j].name
+                phi_j=bn.factors[j]
+                if(name_j in evidence):
+                    a_i[name_j]=evidence[name_j]
+                    b_j=a_i.select(phi_j.variable_names())
+                    w_i*= phi_j.get(a_i.select(b_j))
+                else:
+                    phi_j.condition(a_i)
+                    a_i[name_j]=phi_j.rand()[name_j]
+            b_i=a_i.select(variables=query)
+            hist_i=table.get(b_i)
+            table.set(b_i,hist_i+w)
+        factor=core.Factor(variables=s_vars,
+                           table=table)
+        return factor.normalize()
+        
 class VariableElimination(object):
     def __init__(self,ordering):
         self.ordering=ordering
